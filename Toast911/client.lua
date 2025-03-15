7c704abfe4fa2f4ffbc2afbd594d5e4b978cbaf7
YourDevKyle <122756752+Kylebeast@users.noreply.github.com>
2025-03-15 15:05:36 -0500

    client.lua

--- Toast911/client.lua
+++ Toast911/client.lua
@@ -10,14 +10,14 @@ end)
 RegisterNetEvent("add911Blip")
 AddEventHandler("add911Blip", function(coords, callType)
     local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
-    SetBlipSprite(blip, 161) -- Shield with a star icon
-    SetBlipColour(blip, 1) -- Red color
+    SetBlipSprite(blip, 161) 
+    SetBlipColour(blip, 1) 
     SetBlipScale(blip, 1.2)
     SetBlipAsShortRange(blip, false)
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentSubstringPlayerName(callType .. " Call")
     EndTextCommandSetBlipName(blip)
     PlaySoundFrontend(-1, "TIMER_STOP", "HUD_MINI_GAME_SOUNDSET", true)
-    Citizen.Wait(300000) -- 5 minutes
+    Citizen.Wait(300000) 
     RemoveBlip(blip)
 end)
